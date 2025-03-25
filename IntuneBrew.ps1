454ef434096565dda08fa10e0657d51300d23928
paul0x1 <paul.atkinson@ic-consult.com>
2025-03-25 16:01:21 +0000

    Update IntuneBrew.ps1

--- IntuneBrew.ps1
+++ IntuneBrew.ps1
@@ -472,7 +472,7 @@ function Add-IntuneAppLogo {
         else {
             # Try to download from repository
             $logoFileName = $appName.ToLower().Replace(" ", "_") + ".png"
-            $logoUrl = "https://raw.githubusercontent.com/ugurkocde/IntuneBrew/main/Logos/$logoFileName"
+            $logoUrl = "https://github.com/paul0x1/IntuneBrew/blob/testing/Logos/$logoFileName"
             Write-Host "Downloading logo from: $logoUrl" -ForegroundColor Gray
             
             # Download the logo
@@ -706,7 +706,7 @@ if ($LocalFile) {
 }
 
 # Fetch supported apps from GitHub repository
-$supportedAppsUrl = "https://raw.githubusercontent.com/ugurkocde/IntuneBrew/refs/heads/main/supported_apps.json"
+$supportedAppsUrl = "https://github.com/paul0x1/IntuneBrew/blob/testing/supported_apps.json"
 $githubJsonUrls = @()
 
 try {
@@ -866,7 +866,7 @@ function Is-ValidUrl {
         [string]$url
     )
 
-    if ($url -match "^https://raw.githubusercontent.com/ugurkocde/IntuneBrew/main/Apps/.*\.json$") {
+    if ($url -match "^https://github.com/paul0x1/IntuneBrew/blob/testing/Apps/.*\.json$") {
         return $true
     }
     else {
